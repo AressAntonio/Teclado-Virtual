9cfbb1ee977c0d2f688a99020fd1df1cac880670
Antonio_Garrido <99376135+AressAntonio@users.noreply.github.com>
2023-01-18 18:05:13 -0600

    inicial

---
+++ README.md
@@ -0,0 +1,37 @@
+# Proyecto Teclado||Virtual
+
+<p>Aplicacion web que muestra en pantalla un teclado virtual<br>asi como una caja de texto que va mostrando la tecla que se vaya presionando en el teclado</p><br>
+
+<h3>Tecnologias Utlizadas:</h3>
+<ul>
+	<li>HTML</li>
+	<li>CSS</li>
+	<li>JS</li>
+</ul><br>
+
+<h3>Modo de uso:</h3>
+<ul>
+	<ol>Descargar repo del proyecto.</ol>
+	<ol>Abrir carpeta del proyecto.</ol>
+	<ol>Abrir script index.html con navegador de su eleccion.</ol>
+</ul><br>
+
+<table>
+	<tr>
+		<th>Vista previa.</th>
+	</tr>
+	<tr>
+		<td>
+			<img src="" alt="" width="400">
+		</td>
+		<td>
+			<img src="" alt="" width="400">
+		</td>
+		<td>
+			<img src="" alt="" width="400">
+		</td>
+		<td>
+			<img src="" alt="" width="400">
+		</td>
+	</tr>
+</table>
---
+++ src/js/index.js
@@ -0,0 +1,142 @@
+//creacion de arreglo 
+//que contendra los botones del teclado.
+
+const keys = [
+	[
+		['1', '!'],
+		['2', '@'],
+		['3', '#'],
+		['4', '$'],
+		['5', '%'],
+		['6', '&'],
+		['7', '/'],
+		['8', '('],
+		['9', ')'],
+		['0', '='],
+		['´', '?'],
+		['¡', '¿']
+	],//primera fila del teclado
+	[
+		['q', 'Q'],
+		['w', 'W'],
+		['e', 'E'],
+		['r', 'R'],
+		['t', 'T'],
+		['y', 'Y'],
+		['u', 'U'],
+		['i', 'I'],
+		['o', 'O'],
+		['p', 'P'],
+		['`', '^'],
+		['+', '*'],
+	],//segunda fila del teclado
+	[
+		['MAYUS', 'MAYUS'],
+		['a', 'A'],
+		['s', 'S'],
+		['d', 'D'],
+		['f', 'F'],
+		['g', 'G'],
+		['h', 'H'],
+		['j', 'J'],
+		['k', 'K'],
+		['l', 'L'],
+		['ñ', 'Ñ'],
+		['"', '{'],
+		['~', '}'],
+	],//tercer fila del teclado
+	[
+		['SHIFT', 'SHIFT'],
+		['<', '>'],
+		['z', 'Z'],
+		['x', 'X'],
+		['c', 'C'],
+		['v', 'V'],
+		['b', 'B'],
+		['n', 'N'],
+		['m', 'M'],
+		[',', ':'],
+		['.', ';'],
+		['-', '_'],
+		
+	],//cuarta fila del teclado
+	[
+		['SPACE', 'space'],
+	] //ultima fila del teclado
+];
+
+
+let mayus = false;
+let shift = false;
+let current = null;
+
+renderKeyboard();
+
+function renderKeyboard() {
+  const keyboardContainer = document.querySelector("#keyboard-container");
+  let empty = `<div class="key-empty"></div>`;
+  const layers = keys.map((layer, i) => {
+    return layer.map((key) => {
+      if (key[0] === "SHIFT")
+        return `<button class="key key-shift ${shift ? "activated" : ""}">${key[0]}</button>`;
+      if (key[0] === "MAYUS")
+        return `<button class="key key-mayus ${mayus ? "activated" : ""}">${
+          key[0]
+        }</button>`;
+      if (key[0] === "SPACE") return `<button class="key key-space"></button>`;
+      return `<button class="key key-normal">${
+        shift
+          ? key[1]
+          : mayus &&
+            key[0].toLowerCase().charCodeAt(0) >= 97 &&
+            key[0].toLowerCase().charCodeAt(0) <= 122
+          ? key[1]
+          : key[0]
+      }</button>`;
+    });
+  });
+
+  layers[0].push(empty);
+  layers[1].unshift(empty);
+  console.log(layers);
+
+  const htmlLayers = layers.map((layer) => {
+    return layer.join("");
+  });
+  keyboardContainer.innerHTML = "";
+  htmlLayers.forEach((layer) => {
+    keyboardContainer.innerHTML += `<div class="layer">${layer}</div>`;
+  });
+
+  document.querySelectorAll(".key").forEach((key) => {
+    key.addEventListener("click", (e) => {
+      if (current) {
+        if (key.textContent === "SHIFT") {
+          shift = !shift;
+          renderKeyboard();
+        } else if (key.textContent === "MAYUS") {
+          mayus = !mayus;
+          renderKeyboard();
+        } else if (key.textContent === "") {
+          debugger;
+          current.value += " ";
+        } else {
+          current.value += key.textContent;
+          if (shift) {
+            shift = false;
+            renderKeyboard();
+          }
+        }
+        current.focus();
+      }
+    });
+  });
+
+  
+}
+
+document.querySelectorAll("input").forEach((input) => {
+    input.addEventListener("focusin", (e) => {
+      current = e.target;
+    });
+  });
